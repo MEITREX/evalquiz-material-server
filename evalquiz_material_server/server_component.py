ed4f4a0a5683d469213a119c5f626e5a474d6a4c
st170001 <st170001@stud.uni-stuttgart.de>
2023-07-13 22:52:35 +0900

    Start of implementation using the betterproto Python library to generate classes from Protobuf

---
+++ evalquiz_material_server/server_component.py
@@ -0,0 +1,38 @@
+import asyncio
+from shared_classes.generated import MaterialServerBase, Empty, ListOfStrings, MaterialUploadData, String, LectureMaterial
+from grpclib.server import Server
+from typing import AsyncIterator
+from shared_classes.internal_material_controller import InternalMaterialController
+
+
+class MaterialServerService(MaterialServerBase):
+
+    def __init__(self):
+        self.internal_material_controller = InternalMaterialController()
+
+    async def upload_material(
+        self, material_upload_data_iterator: AsyncIterator["MaterialUploadData"]
+    ) -> "Empty":
+        raise grpclib.GRPCError(grpclib.const.Status.UNIMPLEMENTED)
+
+    async def delete_material(self, string: "String") -> "Empty":
+        self.internal_material_controller.delete_material(string)
+        return Empty()
+
+    async def get_material_hashes(self, empty: "Empty") -> "ListOfStrings":
+        material_hashes = self.internal_material_controller.get_material_hashes
+        return ListOfStrings(material_hashes)
+
+    async def get_material(self, string: "String") -> "LectureMaterial":
+        return self.internal_material_controller.get_material_from_hash(string)
+
+
+async def main():
+    server = Server([MaterialServerService()])
+    await server.start("127.0.0.1", 50051)
+    await server.wait_closed()
+
+
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
