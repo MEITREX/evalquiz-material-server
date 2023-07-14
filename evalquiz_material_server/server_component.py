e6ab450a358c635f1acb2ff3f1cd16efa8b3e276
st170001 <st170001@stud.uni-stuttgart.de>
2023-07-14 22:11:22 +0900

    Idea of material server synchronized upload

--- evalquiz_material_server/server_component.py
+++ evalquiz_material_server/server_component.py
@@ -1,4 +1,9 @@
 import asyncio
+from evalquiz_proto.shared.exceptions import (
+    DataChunkNotBytesException,
+    EmptyUploadException,
+    FirstDataChunkNotLectureMaterialException,
+)
 from evalquiz_proto.shared.generated import (
     MaterialServerBase,
     Empty,
@@ -8,18 +13,38 @@ from evalquiz_proto.shared.generated import (
     LectureMaterial,
 )
 from grpclib.server import Server
-from typing import AsyncIterator
+from typing import AsyncIterator, List, Union
 from evalquiz_proto.shared import InternalMaterialController
+import betterproto
 
 
 class MaterialServerService(MaterialServerBase):
+    """Serves endpoints for material manipulation."""
+
     def __init__(self):
         self.internal_material_controller = InternalMaterialController()
 
     async def upload_material(
         self, material_upload_data_iterator: AsyncIterator["MaterialUploadData"]
     ) -> "Empty":
-        raise grpclib.GRPCError(grpclib.const.Status.UNIMPLEMENTED)
+        material_upload_data = [gen async for gen in material_upload_data_iterator()]
+        self._validate_material_upload_data(material_upload_data)
+        lecture_material = material_upload_data[0]
+        for data in material_upload_data[1:]:
+            with open("file.txt", "wb") as binary_file:
+                binary_file.write(data)
+
+    def _validate_material_upload_data(material_upload_data: List[Union[LectureMaterial, bytes]]) -> None:
+        if len(material_upload_data) != 0:
+            lecture_material = material_upload_data[0]
+            type = betterproto.which_one_of(lecture_material, "material_upload_data")[0]
+            if type != "lecture_material":
+                FirstDataChunkNotLectureMaterialException()
+            for data in material_upload_data[1:]:
+                if type != "data":
+                    raise DataChunkNotBytesException()
+        else:
+            raise EmptyUploadException()
 
     async def delete_material(self, string: "String") -> "Empty":
         self.internal_material_controller.delete_material(string)
