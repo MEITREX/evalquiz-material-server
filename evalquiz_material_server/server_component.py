a8b2b80805d0dd845468ef005f01f2e33d012493
st170001 <st170001@stud.uni-stuttgart.de>
2023-07-17 22:24:50 +0900

    Docstring template generation in Google format and additional docstrings

--- evalquiz_material_server/server_component.py
+++ evalquiz_material_server/server_component.py
@@ -28,6 +28,18 @@ class MaterialServerService(MaterialServerBase):
     async def upload_material(
         self, material_upload_data_iterator: AsyncIterator["MaterialUploadData"]
     ) -> "Empty":
+        """Asynchronous method that is used by gRPC as an endpoint.
+        Manages a lecture material upload.
+
+        Args:
+            material_upload_data_iterator (AsyncIterator[MaterialUploadData]): Included LectureMaterial as the first element and data in bytes as the following elements.
+
+        Raises:
+            FirstDataChunkNotLectureMaterialException: Raised, if the first element is not a LectureMaterial.
+
+        Returns:
+            Empty: Empty gRPC compatible return format. Equivalent to "None".
+        """
         material_upload_data = await material_upload_data_iterator.__anext__()
         (type, lecture_material) = betterproto.which_one_of(
             material_upload_data, "material_upload_data"
@@ -42,18 +54,46 @@ class MaterialServerService(MaterialServerBase):
         raise FirstDataChunkNotLectureMaterialException()
 
     async def delete_material(self, string: "String") -> "Empty":
+        """Asynchronous method that is used by gRPC as an endpoint.
+        Manages deletion of lecture materials
+
+        Args:
+            string (String): The hash of the lecture material.
+
+        Returns:
+            Empty: Empty gRPC compatible return format. Equivalent to "None".
+        """
         self.internal_material_controller.delete_material(string.value)
         return Empty()
 
     async def get_material_hashes(self, empty: "Empty") -> "ListOfStrings":
+        """Asynchronous method that is used by gRPC as an endpoint.
+        Returns hashes of all registered lecture materials.
+
+        Args:
+            empty (Empty): Empty gRPC compatible return format. Equivalent to "None". Required as parameter.
+
+        Returns:
+            ListOfStrings: Hashes of all registered lecture materials.
+        """
         material_hashes = self.internal_material_controller.get_material_hashes()
         return ListOfStrings(material_hashes)
 
     async def get_material(self, string: "String") -> "LectureMaterial":
+        """Asynchronous method that is used by gRPC as an endpoint.
+        Returns a specific lecture material for a hash.
+
+        Args:
+            string (String): Hash of the material to query.
+
+        Returns:
+            LectureMaterial: LectureMaterial referenced by the hash.
+        """
         return self.internal_material_controller.get_material_from_hash(string.value)
 
 
 async def main() -> None:
+    """Sets up and starts MaterialServerService."""
     material_storage_path = Path(__file__).parent / "lecture_materials"
     if not os.path.exists(material_storage_path):
         os.makedirs(material_storage_path)
@@ -63,5 +103,6 @@ async def main() -> None:
 
 
 if __name__ == "__main__":
+    """Prevents accidental execution of the server via import."""
     loop = asyncio.get_event_loop()
     loop.run_until_complete(main())
