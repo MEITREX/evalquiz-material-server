ecf414afef94c9f01c04d8bc79866cb69ca98fcb
st170001 <st170001@stud.uni-stuttgart.de>
2023-07-24 23:49:28 +0200

    Changed import path of Sphinx docs

--- docs/conf.py
+++ docs/conf.py
@@ -2,7 +2,7 @@ import os
 import sys
 import sphinx_rtd_theme
 
-sys.path.insert(0, os.path.abspath('../'))
+sys.path.insert(0, os.path.abspath('../evalquiz_material_server'))
 
 extensions = ["sphinx.ext.autodoc", "sphinx.ext.napoleon", "sphinx_rtd_theme"]
 
