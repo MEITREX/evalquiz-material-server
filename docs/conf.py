e16bce5b00981a360e678076da9161a021cf3e63
st170001 <st170001@stud.uni-stuttgart.de>
2023-07-25 00:02:06 +0200

    Sphinx fixed sys paths

--- docs/conf.py
+++ docs/conf.py
@@ -2,7 +2,7 @@ import os
 import sys
 import sphinx_rtd_theme
 
-sys.path.insert(0, os.path.abspath('../evalquiz_material_server'))
+sys.path.insert(0, os.path.abspath('..'))
 
 extensions = ["sphinx.ext.autodoc", "sphinx.ext.napoleon", "sphinx_rtd_theme"]
 
